0b1ed7332f628b24ee721728bd310e56b6cf428c
Neterich04 <sebastian.seifert.isernhagen@gmail.com>
2026-06-09 21:30:39 +0200

    Revert LogisticRegression to C=1.0 (L2 penalty)

--- Uncertainty_Quantification.py
+++ Uncertainty_Quantification.py
@@ -813,8 +813,8 @@ def run_single_test(func_dict, func_name, seed, approaches):
 
         # Sigmoid Calibration (Platt Scaling) to map epistemic uncertainty to OOD probability
         try:
-            # Disable regularization to ensure scale invariance across UQ methods
-            lr = LogisticRegression(C=np.inf)
+            # Revert to standard L2 regularization (C=1.0)
+            lr = LogisticRegression(C=1.0)
             lr.fit(u_e.reshape(-1, 1), y_true_binary)
             p_calibrated = lr.predict_proba(u_e.reshape(-1, 1))[:, 1]
         except Exception:
