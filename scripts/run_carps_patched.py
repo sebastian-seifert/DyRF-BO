15f3cd7fbd81a140c3d3bfbd884387b8303ef9b2
Neterich04 <sebastian.seifert.isernhagen@gmail.com>
2026-07-14 15:19:40 +0200

    Make check_help monkey-patch conditional for Python compatibility

--- scripts/run_carps_patched.py
+++ scripts/run_carps_patched.py
@@ -2,13 +2,14 @@ import argparse
 
 # Monkey-patch argparse.ArgumentParser._check_help to bypass compatibility crashes
 # with older Hydra-Core (LazyCompletionHelp) under Python 3.14.
-original_check_help = argparse.ArgumentParser._check_help
-def custom_check_help(self, action):
-    try:
-        original_check_help(self, action)
-    except Exception:
-        pass
-argparse.ArgumentParser._check_help = custom_check_help
+if hasattr(argparse.ArgumentParser, "_check_help"):
+    original_check_help = argparse.ArgumentParser._check_help
+    def custom_check_help(self, action):
+        try:
+            original_check_help(self, action)
+        except Exception:
+            pass
+    argparse.ArgumentParser._check_help = custom_check_help
 
 from carps.run import main
 
